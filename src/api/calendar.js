5dc696ae547d499cfed7d9652db2e3b8b547602d
KyoungTae <117700295+Moyahojang@users.noreply.github.com>
2023-02-24 16:33:33 +0900

    limit

--- src/api/calendar.js
+++ src/api/calendar.js
@@ -3,12 +3,12 @@ import instance from '../shared/axios';
 export const calendarApis = {
   createDailyFood: async data => await instance.post('schedules/excel', data),
   getDailyFood: async (size, page) =>
-    await instance.get(`schedules/all?size=${size}&page=${page}`),
+    await instance.get(`schedules/all?limit=${size}&page=${page}`),
   getRecommnadDailyFood: async (startDate, size, page) =>
     await instance.get(`schedules/recommends`, {
       params: {
         startDate: startDate,
-        size: size,
+        limit: size,
         page: page,
       },
     }),
