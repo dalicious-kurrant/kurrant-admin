1d5f441f2c1c8c0330881ffb3534179521c28d96
jaesincho <jaesin.cho@dalicious.co>
2023-03-02 20:11:35 +0900

    스팟정보 필드 수정

--- src/data/test.js
+++ src/data/test.js
@@ -47,3 +47,30 @@ spotName: '3층';
 updatedDateTime: '2023-02-14';
 zipCode: '10002';
 19;
+
+// 바뀐 데이터
+
+address1: '서울 강남구 역삼로 168';
+address2: '팁스타운S6';
+breakfastDeliveryTime: '09:00';
+breakfastLastOrderTime: '09:00';
+breakfastSupportPrice: null;
+breakfastUseDays: '월, 화, 수, 목, 금';
+createdDateTime: '2023-03-01';
+diningType: '아침, 점심, 저녁';
+dinnerDeliveryTime: '18:00';
+dinnerLastOrderTime: '15:00';
+dinnerSupportPrice: null;
+dinnerUseDays: '월, 화, 수, 목, 금';
+groupId: 1;
+groupName: '달리셔스1';
+id: 19;
+location: null;
+lunchDeliveryTime: '09:00';
+lunchLastOrderTime: '09:00';
+lunchSupportPrice: null;
+lunchUseDays: '월, 화, 수, 목, 금';
+spotId: 26;
+spotName: '달리셔스1';
+updatedDateTime: '2023-03-02';
+zipCode: null;
--- src/pages/customer/SpotInfo/SpotInfoData.js
+++ src/pages/customer/SpotInfo/SpotInfoData.js
@@ -13,13 +13,16 @@ export const SpotInfoRegisterFieldsToOpen = {
   breakfastDeliveryTime: '배송시아침', //  "00:00:00"형식 준수
   breakfastSupportPrice: '지원아침', // null가능number, 0 가능
   breakfastUseDays: '주문요일아침', //null가능 빈칸 가능 월, 수 금
+  breakfastLastOrderTime: '아침주문마감시간',
   lunchDeliveryTime: '배송시간점심', // null가능"00:00:00"형식 준수
   lunchSupportPrice: '지원금점심', // null가능number 0가능
   lunchUseDays: '주문요일점심', //null가능 빈 칸 가능 "월, 화 수목금"
+  lunchLastOrderTime: '점심주문마감시간',
   dinnerDeliveryTime: '배송시간저녁', // null 가능
   dinnerSupportPrice: '지원금저녁', // null 가능
   dinnerUseDays: '주문요일저녁', // null 가능
-  // lastOrderTime: '주문마감시간', //  "00:00:00"형식 준수
+  dinnerLastOrderTime: '저녁주문마감시간',
+
   createdDateTime: '생성일',
   updatedDateTime: '수정일',
 };
@@ -28,7 +31,6 @@ export const SpotInfoFieldsToOpen = {
   // id: '아이디',
   // groupId: '그룹아이디', // 1, 2, 3
   groupName: '상세스팟 이름',
-
   spotId: '스팟아이디',
   spotName: '스팟이름',
   zipCode: '우편번호', // 빈칸이면 안됨, 00000
@@ -36,18 +38,22 @@ export const SpotInfoFieldsToOpen = {
   address2: '상세주소', // 빈칸 가능
   location: '위치', // 빈칸 가능
   diningType: '식타입', //  1,2,3, 복수선택 가능 스트링임
-  breakfastDeliveryTime: '배송시아침', //  "00:00:00"형식 준수
+  breakfastDeliveryTime: '배송시아침', //  "00:00"형식 준수
   breakfastSupportPrice: '지원아침', // null가능number, 0 가능
   breakfastUseDays: '주문요일아침', //null가능 빈칸 가능 월, 수 금
+  breakfastLastOrderTime: '아침주문마감시간', //  "00:00"형식 준수
   lunchDeliveryTime: '배송시간점심', // null가능"00:00:00"형식 준수
   lunchSupportPrice: '지원금점심', // null가능number 0가능
   lunchUseDays: '주문요일점심', //null가능 빈 칸 가능 "월, 화 수목금"
+  lunchLastOrderTime: '점심주문마감시간',
+
   dinnerDeliveryTime: '배송시간저녁', // null 가능
   dinnerSupportPrice: '지원금저녁', // null 가능
   dinnerUseDays: '주문요일저녁', // null 가능
-  // lastOrderTime: '주문마감시간', //  "00:00:00"형식 준수
-  createdDateTime: '생성일',
-  updatedDateTime: '수정일',
+  dinnerLastOrderTime: '저녁주문마감시간',
+
+  createdDateTime: '생성일', // '2023-03-01';
+  updatedDateTime: '수정일', // '2023-03-02';
 };
 
 const inputType = {
@@ -164,6 +170,16 @@ export const SpotInfoFieldsData = [
     headerWidth: 160,
   },
   {
+    fieldName: 'breakfastLastOrderTime',
+    fieldNameKor: '아침주문마감시간',
+    placeholder: '예) 월, 화, 수',
+    maxCharLength: 40,
+    flex: 1,
+    width: undefined,
+    headerWidth: 160,
+  },
+
+  {
     fieldName: 'lunchDeliveryTime',
     fieldNameKor: '배송시간점심',
     placeholder: ' 예) 00:00:00 ',
@@ -190,6 +206,15 @@ export const SpotInfoFieldsData = [
     headerWidth: 200,
   },
   {
+    fieldName: 'lunchLastOrderTime',
+    fieldNameKor: '점심주문마감시간',
+    placeholder: '예) 00:00:00',
+    maxCharLength: 40,
+    flex: 1,
+    width: undefined,
+    headerWidth: 200,
+  },
+  {
     fieldName: 'dinnerDeliveryTime',
     fieldNameKor: '배송시간저녁',
     placeholder: '예) 00:00:00',
@@ -209,6 +234,14 @@ export const SpotInfoFieldsData = [
   {
     fieldName: 'dinnerUseDays',
     fieldNameKor: '주문요일저녁',
+    placeholder: '월, 화, 수',
+    maxCharLength: 40,
+    flex: 1,
+    width: undefined,
+  },
+  {
+    fieldName: 'dinnerLastOrderTime',
+    fieldNameKor: '저녁주문마감시간',
     placeholder: '월, 화, 수',
     maxCharLength: 40,
     flex: 1,
