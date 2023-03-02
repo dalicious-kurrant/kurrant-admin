d091eaa4511a237f721ceb4d8a82b8c0af841bec
KyoungTae <117700295+Moyahojang@users.noreply.github.com>
2023-03-02 11:48:26 +0900

    결제 페이지

--- src/pages/Success.jsx
+++ src/pages/Success.jsx
@@ -19,9 +19,10 @@ const Success = () => {
 
     const callOrder = async () => {
       alert(JSON.stringify(req));
-      const res = await instanceOrder.post('/users/me/orders', {
-        ...JSON.parse(req),
-      });
+      const res = await instanceOrder.post(
+        '/users/me/orders',
+        JSON.stringify(req),
+      );
       alert(JSON.stringify(res));
       // const res = await successApi.orderSuccess(req);
       window.ReactNativeWebView.postMessage(
